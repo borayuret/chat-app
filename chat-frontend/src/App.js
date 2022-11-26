6ce0c6520df27df891472c77479114ba475b8520
Bora Yuret <borayuret@gmail.com>
2022-11-26 17:56:56 +0300

    added 404 route

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import Chat from './components/Chat/Chat'
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 
 function App() {
@@ -13,7 +13,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Chat />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path='/register' element={<Register />} />      
+
+          <Route path="/404" element={<h1>404 - Page not found!</h1>} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
         </Routes>
       </BrowserRouter>
     </div>
